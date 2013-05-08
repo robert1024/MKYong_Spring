49c5e5ef4fc72acd62fd5f321f50d925373b6db6
robert1024 <shiyuanhai@gmail.com>
2013-05-08 17:24:22 -0400

    Spring 3 HelloWorld.

---
+++ Spring3Example/src/main/java/com/bigshi/mkyong/spring/App.java
@@ -0,0 +1,18 @@
+package com.bigshi.mkyong.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBean.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+    }
+}
